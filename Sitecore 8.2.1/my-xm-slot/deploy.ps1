1fb0df74ee665c5b6ae0a683f5e6431486958e5d
mathieu-benoit <mathieu-benoit@hotmail.fr>
2017-02-19 22:53:19 -0500

    Initial commit for my-xm-slot (i.e. copy of my-xm).

---
+++ Sitecore 8.2.1/my-xm-slot/deploy.ps1
@@ -0,0 +1,13 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $ResourceGroupLocation = "East US",
+    [string] $TemplateFile = ".\azuredeploy.json",
+    [string] [Parameter(Mandatory=$true)] $SqlServerLogin,
+    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword
+)
+
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubcriptionName "TODO"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation;
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -sqlserverLogin $SqlServerLogin -sqlserverPassword $SqlServerPassword;
+                                    
