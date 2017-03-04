cafd5c72d2a7ef4764e7e0e84301c5e3139e7d64
mathieu-benoit <mathieu-benoit@hotmail.fr>
2017-03-04 10:02:36 -0500

    docs + linked tesmplates for slot.

--- Sitecore 8.2.1/my-xm-slot/deploy.ps1
+++ Sitecore 8.2.1/my-xm-slot/deploy.ps1
@@ -3,11 +3,26 @@ Param(
     [string] $ResourceGroupLocation = "East US",
     [string] [Parameter(Mandatory=$true)] $SlotName,
     [string] $TemplateFile = ".\azuredeploy.json",
+    [string] $LicenseFile = ".\license.xml",
+    [string] $CdMsDeployPackageUrl = 'TODO/xm1/Sitecore%208.2%20rev.%20161221_cd.scwdp.zip',
+    [string] $CmMsDeployPackageUrl = 'TODO/xm1/Sitecore%208.2%20rev.%20161221_cm.scwdp.zip',
     [string] [Parameter(Mandatory=$true)] $SqlServerLogin,
-    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword
+    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword,
+    [securestring] [Parameter(Mandatory=$true)] $SitecoreAdminPassword
 )
+
+$licenseFileContent = Get-Content -Raw -Encoding UTF8 -Path $LicenseFile | Out-String;
+$parameters = New-Object -TypeName Hashtable;
+$parameters.Add("slotName", $SlotName);
+$parameters.Add("cdMsdeployPackageurl", $CdMsDeployPackageUrl);
+$parameters.Add("cmMsdeployPackageurl", $CmMsDeployPackageUrl);
+$parameters.Add("sqlserverLogin", $SqlServerLogin);
+$parameters.Add("sqlserverPassword", $SqlServerPassword);
+$parameters.Add("sitecoreAdminPassword", $SitecoreAdminPassword);
+$parameters.Add("licenseXml", $licenseFileContent);
 
 #Login-AzureRmAccount
 #Select-AzureRmSubscription -SubscriptionName "TODO"
-New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -slotName $SlotName -sqlserverLogin $SqlServerLogin -sqlserverPassword $SqlServerPassword;
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterObject $parameters;
+
                                     
