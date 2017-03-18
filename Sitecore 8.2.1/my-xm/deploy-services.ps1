867d7d09cf67bf2134a3645fbb9d4a9fc362eae9
mathieu-benoit <mathieu-benoit@hotmail.fr>
2017-03-18 19:51:11 -0400

    my-xm - KeyVault integration in ps1

--- Sitecore 8.2.1/my-xm/deploy-services.ps1
+++ Sitecore 8.2.1/my-xm/deploy-services.ps1
@@ -2,11 +2,14 @@ Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
     [string] $ResourceGroupLocation = "East US",
     [string] $TemplateFile = ".\azuredeploy-services.json",
-    [string] [Parameter(Mandatory=$true)] $SqlServerLogin,
-    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword
+    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
+    [string] [Parameter(Mandatory=$true)] $SqlServerLogin
 )
+
+$sqlServerPasswordKeyVaultSecret = Get-AzureKeyVaultSecret -VaultName $SecureKeyVault -Name "SqlServerPassword";
+$sqlServerPassword = ConvertTo-SecureString ($secretSqlPass.SecretValueText) -AsPlainText -Force;
 
 #Login-AzureRmAccount
 #Select-AzureRmSubscription -SubscriptionName "TODO"
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation;
-New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -sqlserverLogin $SqlServerLogin -sqlserverPassword $SqlServerPassword;
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -sqlserverLogin $SqlServerLogin -sqlserverPassword $sqlServerPassword;
--- Sitecore 8.2.1/my-xm/deploy.ps1
+++ Sitecore 8.2.1/my-xm/deploy.ps1
@@ -2,25 +2,44 @@ Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
     [string] $ResourceGroupLocation = "East US",
     [string] $TemplateFile = ".\azuredeploy.json",
-    [string] $LicenseFile = ".\license.xml",
     [string] $CdMsDeployPackageUrl = 'TODO/xm1/Sitecore%208.2%20rev.%20161221_cd.scwdp.zip',
     [string] $CmMsDeployPackageUrl = 'TODO/xm1/Sitecore%208.2%20rev.%20161221_cm.scwdp.zip',
-    [string] [Parameter(Mandatory=$true)] $SqlServerLogin,
-    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword,
-    [securestring] [Parameter(Mandatory=$true)] $SitecoreAdminPassword
+    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
+    [string] [Parameter(Mandatory=$true)] $SqlServerLogin
 )
 
-$licenseFileContent = Get-Content -Raw -Encoding UTF8 -Path $LicenseFile | Out-String;
+Function Unzip
+{
+    param([byte[]]$gzipContent)
+    $input = New-Object System.IO.MemoryStream (,$zipContent); 
+    $gzipStream = New-Object System.IO.Compression.GzipStream ($input, ([IO.Compression.CompressionMode]::Decompress));
+    $output = New-Object System.IO.MemoryStream;
+    $gzipStream.CopyTo($output);
+    return $output.ToArray();
+}
+
+$secretLicense = Get-AzureKeyVaultSecret $VaultName $SecureKeyVault -Name "SitecoreLicense" ;
+$zipContent = [System.Convert]::FromBase64String($secretLicense.SecretValueText);
+$licenseFile = Unzip($zipContent);
+$licenseFileContent = [System.Text.Encoding]::UTF8.GetString($licenseFile);
+
+$sitecoreAdminPasswordKeyVaultSecret = Get-AzureKeyVaultSecret $VaultName $SecureKeyVault -Name "SitecoreAdminPassword";
+$sitecoreAdminPassword = ConvertTo-SecureString ($secretSitecorePass.SecretValueText) -AsPlainText -Force;
+
+$sqlServerPasswordKeyVaultSecret = Get-AzureKeyVaultSecret -VaultName $SecureKeyVault -Name "SqlServerPassword";
+$sqlServerPassword = ConvertTo-SecureString ($secretSqlPass.SecretValueText) -AsPlainText -Force;
+
 $parameters = New-Object -TypeName Hashtable;
 $parameters.Add("cdMsdeployPackageurl", $CdMsDeployPackageUrl);
 $parameters.Add("cmMsdeployPackageurl", $CmMsDeployPackageUrl);
 $parameters.Add("sqlserverLogin", $SqlServerLogin);
-$parameters.Add("sqlserverPassword", $SqlServerPassword);
-$parameters.Add("sitecoreAdminPassword", $SitecoreAdminPassword);
+$parameters.Add("sqlserverPassword", $sqlServerPassword);
+$parameters.Add("sitecoreAdminPassword", $sitecoreAdminPassword);
 $parameters.Add("licenseXml", $licenseFileContent);
 
 #Login-AzureRmAccount
 #Select-AzureRmSubscription -SubscriptionName "TODO"
+
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation;
 New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterObject $parameters;
                                     
