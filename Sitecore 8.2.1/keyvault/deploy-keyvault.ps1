2456544251959e128ee3ccaffd2b6675066a55c9
mathieu-benoit <mathieu-benoit@hotmail.fr>
2017-03-18 19:39:41 -0400

    Add the keyvault creation powershell script.

---
+++ Sitecore 8.2.1/keyvault/deploy-keyvault.ps1
@@ -0,0 +1,30 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
+    [string] $Location = "East US",
+    [string] $LicenseFile = ".\license.xml",
+    [securestring] [Parameter(Mandatory=$true)] $SqlServerPassword,
+    [securestring] [Parameter(Mandatory=$true)] $SitecoreAdminPassword
+)
+
+Function Zip
+{
+    param([byte[]]$content)
+    $output = New-Object System.IO.MemoryStream; 
+    $gzipStream = New-Object System.IO.Compression.GzipStream ($output , ([IO.Compression.CompressionMode]::Compress));
+    $gzipStream.Write($content, 0, $content.Length);
+    $gzipStream.Close();
+    return $output.ToArray();
+}
+
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubscriptionName "TODO"
+#New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location;
+
+New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -Location $Location;
+$zipContent = Zip([IO.File]::ReadAllBytes($LicenseFile));
+$zipString=[System.Convert]::ToBase64String($zipContent);
+$secretLicense = ConvertTo-SecureString $zipString -AsPlainText -Force
+Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name 'SitecoreLicense' -SecretValue $secretLicense;
+Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name 'SqlServerPassword' -SecretValue $SqlServerPassword;
+Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name 'SitecoreAdminPassword' -SecretValue $SitecoreAdminPassword;
